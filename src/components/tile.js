7c28676ea31ea91461a412a317aec6b1e6e22a4e
Toomas Kaarel <toomas.kaarel@gmail.com>
2026-03-05 15:19:24 +0200

    Open tiles

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,12 +1,20 @@
 import './tile.css';
+import { useState } from 'react';
 
 function Tile({x, y}) {
+    // 0=closed tile, 1=open tile
+    const [status, setStatus] = useState(0)
+
     function handleTileClick() {
         console.log(`Clicked tile: x=${x}, y=${y}`)
+        setStatus(1)
     }
 
     return(
-        <div className="tile" onClick={handleTileClick}>
+        <div 
+            className={`tile ${status}`} 
+            onClick={handleTileClick}
+        >
         </div>
     )
 };
