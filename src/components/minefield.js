75de1b4554b437de4c97a615171ad859aa587f97
Toomas Kaarel <toomas.kaarel@gmail.com>
2026-03-10 13:31:31 +0200

    Resetting field

--- src/components/minefield.js
+++ src/components/minefield.js
@@ -1,10 +1,12 @@
 import './minefield.css';
 import Tile from './tile';
 import { shuffle } from '../scripts/shuffleArray';
+import { useRef } from 'react';
 
 function Minefield({mines, width, height}) {
     const tileRows = []; //Rows of tiles
     const minePos = []; //Mines array
+    const tileRefs = useRef([]);
 
     // Create and randomize bombs
     for (let i=0; i < height * width; i++) {
@@ -13,16 +15,25 @@ function Minefield({mines, width, height}) {
     shuffle(minePos);
     console.log(minePos)
 
+    // Resetting the field
+    const handleFieldReset = () => {
+        tileRefs.current.forEach(tile => {
+            if (tile) tile.reset();
+        });
+    };
+
     // Create Minefield
     for (let y=0; y < height; y++) {
         const tiles = []; //Tiles in row
         for (let x=0; x < width; x++) {
+            const tileIndex = y ? `${y}${x}` : `${x}`
             tiles.push(<Tile 
-                key={x}
+                key={tileIndex}
                 x={x}
                 y={y}
                 // Check for mines from mine array
-                mine={minePos[y ? `${y}${x}` : `${x}`] ? 'mine' : ''}
+                mine={minePos[tileIndex] ? 'mine' : ''}
+                ref={(tile) => (tileRefs.current[tileIndex] = tile)}
             />)
         }
         tileRows.push(
@@ -37,6 +48,10 @@ function Minefield({mines, width, height}) {
             <p>Width: {width}, Height: {height}, Mines: {mines}</p> 
             <div className="minefield-container">
                 {tileRows}
+            </div>
+            <div className="controls">
+                <button onClick={() => handleFieldReset()}>{"Reset"}</button>
+                <button>{"(Debug) Reveal mines"}</button>
             </div>
         </div>
     )
--- src/components/tile.js
+++ src/components/tile.js
@@ -1,21 +1,35 @@
 import './tile.css';
-import { useState } from 'react';
+import { useState, useImperativeHandle, forwardRef } from 'react';
 
-function Tile({x, y, mine}) {
+const Tile = forwardRef(({ x, y, mine }, ref) => {
     const [status, setStatus] = useState('closed');
 
     function handleTileClick() {
         console.log(`Clicked tile: x=${x}, y=${y}`)
         setStatus('open')
+        if (mine) {
+            console.log("Boom!!")
+        };
     };
+
+    function onResetField() {
+        setStatus('closed')
+    };
+
+    useImperativeHandle(ref, () => ({
+        reset: onResetField
+    }));
 
     return(
         <div 
             className={`tile ${status} ${mine}`} 
-            onClick={handleTileClick}
+            onClick={() => handleTileClick()}
+            //onClick={() => 
+            //    (status === 'closed' ? handleTileClick() : onResetField())
+            //}
         >
         </div>
     )
-};
+});
 
 export default Tile;
