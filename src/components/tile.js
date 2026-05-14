2257bbd6748c8335c75273b517a09ab460382dbf
Tooma81 <toomas.kaarel@gmail.com>
2026-05-14 13:42:22 +0300

    Mine sprites

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,6 +1,7 @@
 import './tile.css';
 import { useState, useEffect } from 'react';
-import flag from '../assets/flag.png'
+import flagSprite from '../assets/sprites/flag.png'
+import mineSprite from '../assets/sprites/mine.png'
 
 const Tile = ({ 
         id, 
@@ -14,11 +15,17 @@ const Tile = ({
     }) => {
     const [state, setState] = useState('closed'); //Visual state of tile
     const [hasFlag, setHasFlag] = useState(false); // Is flag placed?
+    const [hasMine, setHasMine] = useState(false); // Is open and has mine?
 
     useEffect(()=>{
         setState(status);
         setHasFlag(flagged);
-    }, [status, flagged]) 
+        if (mine && status === 'open') {
+            setHasMine(true);
+        } else {
+            setHasMine(false);
+        }
+    }, [status, flagged, mine]) 
 
     
 
@@ -29,7 +36,10 @@ const Tile = ({
         >
             <p className="indicator">{indicator}</p>
             {hasFlag &&
-                <img src={flag} alt="Flag" className={"flag"}/>
+                <img src={flagSprite} alt="Flag" className={"tile-sprite"}/>
+            }
+            {hasMine && 
+                <img src={mineSprite} alt="Mine" className={"tile-sprite"}/>
             }
         </div>
     )
